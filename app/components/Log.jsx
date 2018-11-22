10353cda926480ad1293a9839a728627bfa420ac
Eric Cambel <cambelem@appstate.edu>
2018-11-21 20:47:54 -0500

    Added a small component.

---
+++ app/components/Log.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Log extends Component {
+
+  render(){
+    return(
+      <div>
+        This is a log component.
+      </div>
+    )
+  }
+};
